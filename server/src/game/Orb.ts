c543789ff018c26003ac92071f8f433d961cf3b9
Pradyumna D P <pradyu.dp@gmail.com>
2026-09-19 12:41:01 +0530

    orb logic

--- server/src/game/Orb.ts
+++ server/src/game/Orb.ts
@@ -13,6 +13,7 @@ export class Orb {
     this.id = `orb_${orbCounter++}`;
     this.x = x;
     this.y = y;
+    // Warm palette so orbs read as "food" distinct from player blobs.
     const hues = [0xffd166, 0x06d6a0, 0xef476f, 0x118ab2];
     this.color = hues[Math.floor(Math.random() * hues.length)];
   }
